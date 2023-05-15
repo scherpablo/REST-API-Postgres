6ac7189dd93f89586b5511ef80f2fb3df9142bfe
scherpablo <scherpablo@gmail.com>
2023-05-15 14:32:32 -0300

    first commit

--- src/database/db.js
+++ src/database/db.js
@@ -1,8 +1,12 @@
 //Importamos Sequelize
 import { Sequelize } from 'sequelize';
+//Importamos dotenv para poder utilizar las variables de entorno
+import dotenv from 'dotenv';
+
+//Instanciamos dotenv
+dotenv.config();
 
 //Creamos un objeto de conexión a la base de datos
-
 export const sequelize = new Sequelize(
     "estudioScherpa",
     "postgres",
